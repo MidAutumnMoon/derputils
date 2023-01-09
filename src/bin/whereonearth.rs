0f30c87ace5294038c40082c759c14ef3366d460
MidAutumnMoon <me@418.im>
2023-01-09 17:52:42 +0800

    whereonearth: inline core logic

--- src/bin/whereonearth.rs
+++ src/bin/whereonearth.rs
@@ -1,5 +1,8 @@
 const PROGRAM_NAME: &str = "whereonearth";
 
+use std::env;
+use std::path::PathBuf;
+
 use anyhow::{
     anyhow,
     Context,
@@ -7,45 +10,28 @@ use anyhow::{
 };
 
 
-/// Lookup `target` inside `path`, follow symlink recursively.
-#[inline]
-fn lookup( target: &str, path: &str ) -> Option<String> {
-
-    use std::path::PathBuf;
-
-    let mut path = PathBuf::from( path );
-
-    path.push( target );
-
-    match path.canonicalize() {
-        Ok( full_path ) => Some( full_path.to_string_lossy().to_string() ),
-        Err( _ ) => None
-    }
-
-}
-
-
 fn main() -> Result<()> {
-
-    use std::env;
 
     let target_program =
         env::args()
         .nth( 1 )
         .ok_or_else( || anyhow!( "{PROGRAM_NAME}: [program name]" ) )?;
 
-
     let env_path =
         env::var( "PATH" ).context( "Failed reading $PATH" )?;
 
-    for path in env_path.rsplit( ':' ) {
-        let lookup_result =
-            lookup( &target_program, path );
 
-        if let Some( full_path ) = lookup_result {
-            println!( "{}", full_path );
+    for path in env_path.rsplit( ':' ) {
+
+        let mut path = PathBuf::from( path );
+
+        path.push( &target_program );
+
+        if let Ok( full_path ) = path.canonicalize() {
+            println!( "{}", full_path.to_string_lossy() );
             return Ok( () )
         }
+
     }
 
 
