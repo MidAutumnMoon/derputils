1000820ca9210eae3587b10e9086ef89d7a4cf76
MidAutumnMoon <me@418.im>
2023-02-05 23:34:23 +0800

    fcombine: improve quality

--- src/bin/fcombine.rs
+++ src/bin/fcombine.rs
@@ -1,11 +1,15 @@
-// fcombine [destination] [origin] ...
-//
-// Combine contents of many files into the destination
-// file without using shell pipelines.
-
-
+#[ allow( dead_code ) ]
 const PROGRAM_NAME: &str = "fcombine";
 
+#[ allow( dead_code ) ]
+const PROGRAM_USAGE: &str =
+    "fcombine [OUTPUT or -] [INPUT]...";
+
+
+use std::{
+    env,
+    path::PathBuf,
+};
 
 use anyhow::{
     bail,
@@ -14,30 +18,81 @@ use anyhow::{
 };
 
 
+
+#[ derive( Debug ) ]
+enum Output {
+    Stdout,
+    File( PathBuf )
+}
+
+impl TryFrom<String> for Output {
+
+    type Error = anyhow::Error;
+
+    fn try_from( value: String ) -> Result<Self, Self::Error> {
+        let stdout_mark = "-";
+
+        if value.is_empty() {
+            bail!( "OUTPUT can not be empty" )
+        } else if value == stdout_mark {
+            Ok( Self::Stdout )
+        } else {
+            Ok( Self::File( PathBuf::from( value ) ) )
+        }
+    }
+
+}
+
+
+#[ derive( Debug ) ]
+struct CmdOptions {
+    output: Output,
+    inputs: Vec<PathBuf>
+}
+
+impl CmdOptions {
+
+    fn from_env() -> Result<Self> {
+
+        let raw_options: Vec<String> =
+            env::args().skip( 1 ).collect();
+
+        if raw_options.len() < 2 {
+            bail!( "Missing required options" )
+        }
+
+        let output = &raw_options[0];
+        let inputs = &raw_options[1..];
+
+        Ok( Self {
+            output: Output::try_from( output.to_owned() )?,
+            inputs: inputs
+                .into_iter()
+                .map( PathBuf::from )
+                .collect()
+        } )
+
+    }
+
+}
+
+
+
 fn main() -> Result<()> {
 
     // Acquire command line options.
 
     let cmd_options =
-        std::env::args()
-        .skip( 1 )
-        .collect::<Vec<String>>();
-
-    if cmd_options.len() < 2 {
-        bail!( "{PROGRAM_NAME}: [destination] [origin] ..." )
-    }
-
-    let destination = &cmd_options[0];
-
-    let origins = &cmd_options[1..];
+        CmdOptions::from_env()
+        .context( PROGRAM_USAGE )?;
 
 
     // Avoid accidents
 
-    use std::path::Path;
-
-    if Path::new( destination ).exists() {
-        bail!( "Something is already here at \"{destination}\"" )
+    if let Output::File( ref path ) = cmd_options.output {
+        if path.try_exists()? {
+            bail!( "Something is already existing at \"{}\"", path.display() )
+        }
     }
 
 
@@ -45,24 +100,29 @@ fn main() -> Result<()> {
 
     use std::fs::OpenOptions;
 
-    let mut opened_destination =
+    let output = match cmd_options.output {
+        Output::Stdout => PathBuf::from( "/dev/stdout" ),
+        Output::File( path ) => path
+    };
+
+    let mut open_output =
         OpenOptions::new()
         .write( true )
         .create( true )
         .truncate( true )
-        .open( destination )
-        .with_context( || format!( "Failed opening \"{destination}\" to write" ) )?;
+        .open( &output )
+        .with_context( || format!( "Failed opening \"{}\" to write", output.display() ) )?;
 
-    for origin in origins {
+    for input in cmd_options.inputs {
 
-        let mut opened_origin =
+        let mut open_input =
             OpenOptions::new()
             .read( true )
-            .open( origin )
-            .with_context( || format!( "Failed opening \"{origin}\" to read" ) )?;
+            .open( &input )
+            .with_context( || format!( "Failed opening \"{}\" to read", &input.display() ) )?;
 
-        std::io::copy( &mut opened_origin, &mut opened_destination )
-            .with_context( || format!( "Error occurred copying \"{origin}\"" ) )?;
+        std::io::copy( &mut open_input, &mut open_output )
+            .with_context( || format!( "Error occurred copying \"{}\"", &input.display() ) )?;
 
     }
 
